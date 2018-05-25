086fd4cdef38d745dfc8007335b0f5cdef13cee7
Shunsuke Kirino <shunsuke.kirino@access-company.com>
2018-05-25 10:26:48 +0900

    [145072] Test: Add some assertions for Testgear.get_env!/1

--- test/gear_config_test.exs
+++ test/gear_config_test.exs
@@ -15,18 +15,22 @@ defmodule Testgear.GearConfigTest do
 
   test "Testgear.get_all_env/0 and get_env/2" do
     assert Cache.write(:testgear, %GearConfig{kv: %{}, domains: [], log_level: :info, alerts: %{}}) == :ok
-    assert Testgear.get_all_env()     == %{}
-    assert Testgear.get_env("foo"   ) == nil
-    assert Testgear.get_env("foo", 0) == 0
+    assert      Testgear.get_all_env()     == %{}
+    assert      Testgear.get_env("foo"   ) == nil
+    assert      Testgear.get_env("foo", 0) == 0
+    catch_error Testgear.get_env!("foo")
 
     assert Cache.write(:testgear, %GearConfig{kv: %{"foo" => 1, "bar" => %{"nested" => "map"}}, domains: [], log_level: :info, alerts: %{}}) == :ok
-    assert Testgear.get_all_env()       == %{"foo" => 1, "bar" => %{"nested" => "map"}}
-    assert Testgear.get_env("foo"     ) == 1
-    assert Testgear.get_env("foo", 0  ) == 1
-    assert Testgear.get_env("bar"     ) == %{"nested" => "map"}
-    assert Testgear.get_env("bar", %{}) == %{"nested" => "map"}
-    assert Testgear.get_env("baz"     ) == nil
-    assert Testgear.get_env("baz", %{}) == %{}
+    assert      Testgear.get_all_env()       == %{"foo" => 1, "bar" => %{"nested" => "map"}}
+    assert      Testgear.get_env("foo"     ) == 1
+    assert      Testgear.get_env("foo", 0  ) == 1
+    assert      Testgear.get_env!("foo")     == 1
+    assert      Testgear.get_env("bar"     ) == %{"nested" => "map"}
+    assert      Testgear.get_env("bar", %{}) == %{"nested" => "map"}
+    assert      Testgear.get_env!("bar")     == %{"nested" => "map"}
+    assert      Testgear.get_env("baz"     ) == nil
+    assert      Testgear.get_env("baz", %{}) == %{}
+    catch_error Testgear.get_env!("baz")
 
     :ets.delete_all_objects(AntikytheraCore.Ets.ConfigCache.table_name())
   end

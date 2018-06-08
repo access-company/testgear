fa739fa61de08e24b7e743b80ad37768c99c9796
Shunpei Shiina <shunpei.shiina@access-company.com>
2018-06-08 12:16:13 +0900

    [133935] use Antikythera.Test.GearConfigHelper.set_config instead of writing to ConfigCache directly

--- test/gear_config_test.exs
+++ test/gear_config_test.exs
@@ -2,25 +2,24 @@
 
 defmodule Testgear.GearConfigTest do
   use ExUnit.Case
-  alias AntikytheraCore.Ets.ConfigCache.Gear, as: Cache
-  alias AntikytheraCore.Config.Gear, as: GearConfig
+  alias Antikythera.Test.GearConfigHelper
 
   setup do
     current_config = Testgear.get_all_env()
     on_exit(fn ->
-      Antikythera.Test.GearConfigHelper.set_config(current_config)
+      GearConfigHelper.set_config(current_config)
       System.delete_env("LOG_LEVEL")
     end)
   end
 
   test "Testgear.get_all_env/0 and get_env/2" do
-    assert Cache.write(:testgear, %GearConfig{kv: %{}, domains: [], log_level: :info, alerts: %{}}) == :ok
+    assert      GearConfigHelper.set_config(%{}) == :ok
     assert      Testgear.get_all_env()     == %{}
     assert      Testgear.get_env("foo"   ) == nil
     assert      Testgear.get_env("foo", 0) == 0
     catch_error Testgear.get_env!("foo")
 
-    assert Cache.write(:testgear, %GearConfig{kv: %{"foo" => 1, "bar" => %{"nested" => "map"}}, domains: [], log_level: :info, alerts: %{}}) == :ok
+    assert      GearConfigHelper.set_config(%{"foo" => 1, "bar" => %{"nested" => "map"}}) == :ok
     assert      Testgear.get_all_env()       == %{"foo" => 1, "bar" => %{"nested" => "map"}}
     assert      Testgear.get_env("foo"     ) == 1
     assert      Testgear.get_env("foo", 0  ) == 1

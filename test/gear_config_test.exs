4c7cc68b81b2cc197f4d1fcfbf4a1fd947c4368d
Shunpei Shiina <s417.lama@gmail.com>
2018-06-15 12:34:36 +0900

    [133935] add a test for checking that gear configs don't change during web request handling (#5)

--- test/gear_config_test.exs
+++ test/gear_config_test.exs
@@ -2,11 +2,13 @@
 
 defmodule Testgear.GearConfigTest do
   use ExUnit.Case
+  alias Antikythera.Httpc
   alias Antikythera.Test.GearConfigHelper
 
   setup do
     current_config = Testgear.get_all_env()
     on_exit(fn ->
+      :ets.delete_all_objects(AntikytheraCore.Ets.ConfigCache.table_name())
       GearConfigHelper.set_config(current_config)
     end)
   end
@@ -29,7 +31,33 @@ defmodule Testgear.GearConfigTest do
     assert      Testgear.get_env("baz"     ) == nil
     assert      Testgear.get_env("baz", %{}) == %{}
     catch_error Testgear.get_env!("baz")
+  end
 
-    :ets.delete_all_objects(AntikytheraCore.Ets.ConfigCache.table_name())
+  test "should not reflect changes in gear configs while handling web requests" do
+    config = %{"foo" => 1, "bar" => %{"nested" => "map"}}
+    assert GearConfigHelper.set_config(config) == :ok
+
+    Process.register(self(), :test_runner)
+    spawn(fn ->
+      res = Req.get("/config_cache")
+      send(:test_runner, {:received_response, res})
+    end)
+
+    assert_receive({:finished_fetching_gear_config, handler_pid}, 5_000)
+
+    GearConfigHelper.set_config(%{"foo" => "not_to_be_cached"})
+    {:dictionary, dict} = Process.info(handler_pid, :dictionary)
+    assert Keyword.has_key?(dict, :gear_configs)
+
+    send(handler_pid, :gear_config_changed)
+
+    assert_receive({:received_response, res}, 5_000)
+    %Httpc.Response{status: status, body: body} = res
+    %{"before" => config_before, "after" => config_after} = Poison.decode!(body)
+    assert status == 200
+    assert config == config_before
+    assert config == config_after
+    {:dictionary, dict} = Process.info(handler_pid, :dictionary)
+    refute Keyword.has_key?(dict, :gear_configs)
   end
 end
---
+++ web/controller/config_cache.ex
@@ -0,0 +1,21 @@
+# Copyright(c) 2015-2018 ACCESS CO., LTD. All rights reserved.
+
+defmodule Testgear.Controller.ConfigCache do
+  use Antikythera.Controller
+
+  def check(conn) do
+    config_before = Testgear.get_all_env()
+    try do
+      send(:test_runner, {:finished_fetching_gear_config, self()})
+    rescue
+      ArgumentError -> :ok
+    end
+    receive do
+      :gear_config_changed -> :ok
+    after
+      5_000 -> :ok
+    end
+    config_after = Testgear.get_all_env()
+    Conn.json(conn, 200, %{before: config_before, after: config_after})
+  end
+end

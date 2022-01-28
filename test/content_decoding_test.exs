976f88b1b476bd4e1d9d3bbd9e698bef3fcf0369
Takaaki Seki <takaaki.seki@access-company.com>
2022-01-28 17:14:19 +0900

    [241420] Test for ContentDecoding plug (#27)

---
+++ test/content_decoding_test.exs
@@ -0,0 +1,37 @@
+# Copyright(c) 2015-2022 ACCESS CO., LTD. All rights reserved.
+
+defmodule Testgear.ContentDecodingTest do
+  use ExUnit.Case
+
+  @path "/content_decoding"
+  @raw_body "foo"
+  @compressed_body :zlib.gzip(@raw_body)
+
+  test "should do nothing if content-encoding header is not present" do
+    headers = %{
+      "content-type" => "text/plain",
+    }
+    res = Req.post(@path, @raw_body, headers, skip_body_decompression: true)
+    assert res.status == 200
+    assert res.body == @raw_body
+  end
+
+  test "should decompress request body with gzip content-encoding header" do
+    headers = %{
+      "content-type" => "text/plain",
+      "content-encoding" => "gzip",
+    }
+    res = Req.post(@path, @compressed_body, headers, skip_body_decompression: true)
+    assert res.status == 200
+    assert res.body == @raw_body
+  end
+
+  test "should return 400 status if the compression fails" do
+    headers = %{
+      "content-type" => "text/plain",
+      "content-encoding" => "gzip",
+    }
+    res = Req.post(@path, @raw_body, headers, skip_body_decompression: true)
+    assert res.status == 400
+  end
+end
---
+++ web/controller/content_decoding.ex
@@ -0,0 +1,13 @@
+# Copyright(c) 2015-2022 ACCESS CO., LTD. All rights reserved.
+
+defmodule Testgear.Controller.ContentDecoding do
+  use Antikythera.Controller
+
+  plug Antikythera.Plug.ContentDecoding, :decode, []
+
+  def echo(conn) do
+    conn
+    |> Conn.put_status(200)
+    |> Conn.resp_body(conn.request.body)
+  end
+end

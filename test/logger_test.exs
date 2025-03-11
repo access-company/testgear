2266ae7ee0777073307316f5e3eafb4c2c334be1
Yuki Sekiguchi <yuki.sekiguchi@access-company.com>
2025-03-11 18:31:23 +0900

    [265200] Make sure that a new rotated log file is created in the test (#49)

--- test/logger_test.exs
+++ test/logger_test.exs
@@ -2,7 +2,7 @@
 
 defmodule Testgear.LoggerTest do
   use ExUnit.Case
-  alias Antikythera.Test.ProcessHelper
+  alias Antikythera.Test.{GearLogHelper, ProcessHelper}
   alias AntikytheraCore.Path, as: CorePath
   alias AntikytheraCore.GearModule
   alias AntikytheraCore.GearLog.Writer
@@ -19,9 +19,21 @@ defmodule Testgear.LoggerTest do
     %{uploader: nil} = :sys.get_state(logger_name)
     {rotated_logs_before, uploaded_logs_before} = find_log_files(log_dir)
 
+    # Make sure that a new rotated file is created and its file name isn't conflicted with existing ones.
+    # FileHandle.rotated_file_path/1 uses current seconds as a suffix of the file name.
+    # We need to 1 second sleep to avoid the confliction.
+    GearLogHelper.set_context_id()
+    Testgear.Logger.error("ensure log to be rotated")
+    :timer.sleep(1000)
+
+    # This should create a new rotated log file and start uploading it because there are logs in the current log file.
     assert Writer.rotate_and_start_upload_in_all_nodes(:testgear) == :abcast
     %{uploader: uploader} = :sys.get_state(logger_name)
     assert is_pid(uploader)
+    # This shouldn't create a new rotated log file because there are no logs in the current log file.
+    # But, this may be flaky because other process may write logs to the current log file.
+    # In this case, AntikytheraEal.LogStorage.FileSystem.upload_rotated_logs/1 may upload this log file
+    # depending on where upload_rotated_logs/1 is processing.
     assert Writer.rotate_and_start_upload_in_all_nodes(:testgear) == :abcast
     %{uploader: ^uploader} = :sys.get_state(logger_name) # don't spawn multiple uploader processes simultaneously
     ProcessHelper.monitor_wait(uploader)
